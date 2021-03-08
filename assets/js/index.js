3390ac8d840a5378c546dc6a71cdd2a94658c568
Erik <62840687+nausea87@users.noreply.github.com>
2021-03-08 15:25:51 +0100

    lots of formatting

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,24 @@
-// function clickButton() {
+function clickButton() {
     
-//     document.getElementById("demo").innerHTML = "Comming soon!";
-//     console.log("I clicked the button!");
-// }
+    document.getElementById("demo").innerHTML = "Comming soon!";
+    console.log("I clicked the button!");
+}
 
-const screenClick = addEventListener("click", () => {
-    // What happens when you click
-        console.log("Clickin' on the screen works!");
-    });
+// const screenClick = addEventListener("click", () => {
+//     // What happens when you click
+//         console.log("Clickin' on the screen works!");
+//     });
     
     
-    window.addEventListener('scroll', () => {
-        //What happens when you scroll
-        console.log('log scroll from top:', window.screenY);
-    });
+//     window.addEventListener('scroll', () => {
+//         //What happens when you scroll
+//         console.log('log scroll from top:', window.screenY);
+//     });
 
-    window.addEventListener('keypress', (event) => {
-        // Logs what key you're pressing!
-        console.log("You've pressed the key:", event.key);
-    });
+//     window.addEventListener('keypress', (event) => {
+//         // Logs what key you're pressing!
+//         console.log("You've pressed the key:", event.key);
+//     });
       
     
  
