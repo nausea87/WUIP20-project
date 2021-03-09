0499c758a028080f0e633b524749915ab13e9033
Erik <62840687+nausea87@users.noreply.github.com>
2021-03-09 16:28:50 +0100

    should i tailwind???

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,49 @@
-function clickButton() {
-    
-    document.getElementById("demo").innerHTML = "Comming soon!";
-    console.log("I clicked the button!");
-}
+// const { doc } = require("prettier");
 
-// const screenClick = addEventListener("click", () => {
-//     // What happens when you click
-//         console.log("Clickin' on the screen works!");
-//     });
-    
-    
-//     window.addEventListener('scroll', () => {
-//         //What happens when you scroll
-//         console.log('log scroll from top:', window.screenY);
-//     });
+const buttonClick = document.querySelector('button');
+buttonClick.addEventListener('click', () => {
+    console.log('Hello');
+ 
+});
 
-//     window.addEventListener('keypress', (event) => {
-//         // Logs what key you're pressing!
-//         console.log("You've pressed the key:", event.key);
-//     });
-      
+
+const foxClick = document.querySelector('.hare-and-fox-img');
+foxClick.addEventListener("click", () => {
+   console.log("Clickin' on the image works!");
+});
+
+const forestClick = document.querySelector('.skog-img');
+forestClick.addEventListener("click", () => {
+  console.log("Clickin' on the trees works!");
+});
     
+// Simple title animation
  let i = 0;
  setInterval(function () {
      document.title = i++ % 2 == 0 ? 'Welcome to...' : "Skogsfesten 2022";
- }, 2000);
+ }, 3000);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ // window.addEventListener('scroll', () => {
+    //     //What happens when you scroll
+    //     console.log("log scroll from top:", window.screenY);
+    // });
+
+    // window.addEventListener('keypress', (event) => {
+    //     // Logs what key you're pressing!
+    //     console.log("You've pressed the key:", event.key);
+    // });
+      
+ 
