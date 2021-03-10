3dac251649d8f0e1677b618946d3ec4bd8be2700
Erik <62840687+nausea87@users.noreply.github.com>
2021-03-10 16:54:35 +0100

    animation brainstorming

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,21 @@
 // const { doc } = require("prettier");
 
-const buttonClick = document.querySelector('button');
-buttonClick.addEventListener('click', () => {
-    console.log('Hello');
+// const buttonClick = document.querySelector('button');
+// buttonClick.addEventListener('click', () => {
+//    console.log(window.location.search);
  
-});
+// });
 
 
-const foxClick = document.querySelector('.hare-and-fox-img');
-foxClick.addEventListener("click", () => {
-   console.log("Clickin' on the image works!");
-});
+// const foxClick = document.querySelector('.hare-and-fox-img');
+// foxClick.addEventListener("click", () => {
+//    console.log("Clickin' on the image works!");
+// });
 
-const forestClick = document.querySelector('.skog-img');
-forestClick.addEventListener("click", () => {
-  console.log("Clickin' on the trees works!");
-});
+// const forestClick = document.querySelector('.skog-img');
+// forestClick.addEventListener("click", () => {
+//   console.log("Clickin' on the trees works!");
+// });
     
 // Simple title animation
  let i = 0;
@@ -23,7 +23,39 @@ forestClick.addEventListener("click", () => {
      document.title = i++ % 2 == 0 ? 'Welcome to...' : "Skogsfesten 2022";
  }, 3000);
  
- 
+
+// const buttonHover = document.querySelector('button');
+// buttonClick.addEventListener('mouseover', () => {
+//     console.log('Hovering');
+// });
+
+
+
+ //////////////////////////////// URL.js
+
+//  const dynamicName = document.querySelector(".dynamic-name");
+//  const dynamicEmail = document.querySelector(".dynamic-email");
+//  const welcome = document.querySelector(".welcome");
+
+//  function getUrlParameter(name) {
+//      name = name.replace(/[\[]/,  "\\[").replace(/[\]]/,  "\\]");
+//      var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+//      var results = regex.exec(location.search);
+//      return results === null
+//      ? ""
+//      : decodeURIComponent(results[1].replace(/\+/g, " "));
+//  }
+
+//  function capitalize(string) {
+//      return string.charAt(0).toUpperCase() + string.slice(1);
+//       }
+
+//       dynamicName.innerHTML = capitalize(getUrlParameter("name"));
+//       dynamicEmail.setAttribute("value", getUrlParameter("email"));
+
+//       if (!getUrlParameter("name")) {
+//           welcome.style.display = "none";
+//       }
  
  
  
