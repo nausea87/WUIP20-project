518ca0b2c7e3868e3423722a5bc21a9e60bbe13e
Erik <62840687+nausea87@users.noreply.github.com>
2021-03-17 19:09:11 +0100

    vip status

--- assets/js/query.js
+++ assets/js/query.js
@@ -2,20 +2,28 @@ let urlParams = new URLSearchParams(window.location.search);
 
 const welcome = document.querySelector('.welcome');
 const nameFiller = document.querySelector('#name');
+const vip = document.querySelector('.vip');
 
 if (urlParams.has('name' && 'email')) {
     let name = urlParams.get('name');
-    let email = urlParams.get('email');    
-    // Email here is actually the lastname, due to me explaining 
-    // poorly, and Evelina had already finished the QR codes
-    // with the lastnames inside email parameters :) 
+    let lastname = urlParams.get('email');    
+    // Email url param here is actually functioning as the lastname, 
+    // due to me explaining poorly, and Evelina had already
+    // finished the QR codes with the lastnames inside
+    // email parameters :) 
     // This is a temporary fix for making it 'look right'.
 
     // TLDR; Email param is used as lastname.
-    welcome.innerText += `Välkommen ${name} ${email}`;
-    nameFiller.setAttribute('value', name);   
+    welcome.innerText += `Välkommen ${name} ${lastname}`;
+    nameFiller.setAttribute('value', name);
+        
+    if (name == 'Erik' && lastname == 'White') {
+        vip.innerText += " Status: VIP";
+    }
+    if (name == 'Evelina' && lastname == 'Bakos') {
+        vip.innerText += " Status: VIP";
+    }      
 }
-
 const favAnimal = document.querySelector('.animal');
 
 if(urlParams.has('animal')) {
@@ -24,6 +32,4 @@ if(urlParams.has('animal')) {
 }
 
 //TODO: Post animals like cat,dog, cow etc as an icon.
-
-
 // Phone visible atm for some sort of logic on site.
